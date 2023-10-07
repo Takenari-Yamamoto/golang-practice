40a3a21ca52d3588858194954b71e1d965682156
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-10-07 09:31:58 +0900

    あかんやつ

--- gql-practice/domain/user.go
+++ gql-practice/domain/user.go
@@ -1,6 +1,33 @@
 package domain
 
+import (
+	"math/rand"
+	"time"
+)
+
 type User struct {
 	ID   string `json:"id"`
 	Name string `json:"username"`
+}
+
+const (
+	USER_ID_TKNR1216   = "tknr1216"
+	USER_ID_TAKASHI003 = "takashi003"
+	USER_ID_TARO115    = "taro115"
+	USER_ID_HANAKO43   = "hanako43"
+	USER_ID_YAMADA234  = "yamada234"
+)
+
+func GenerateUserId() string {
+	userIds := []string{
+		USER_ID_TKNR1216,
+		USER_ID_TAKASHI003,
+		USER_ID_TARO115,
+		USER_ID_HANAKO43,
+		USER_ID_YAMADA234,
+	}
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return userIds[r.Intn(len(userIds))]
+
 }
