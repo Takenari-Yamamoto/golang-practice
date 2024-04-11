7045f6f80b94150f1ba53a188d7a2b1a77950a0f
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2024-04-11 10:46:30 +0900

    fix

--- golang-ecs/app/cmd/main.go
+++ golang-ecs/app/cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
