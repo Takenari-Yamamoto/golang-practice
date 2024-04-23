f162324ca7a21753add489964f2533e75679d572
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2024-04-24 08:39:11 +0900

    fix バケット

---
+++ infra/golang-study/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "golang-study-terraform-state"
+    key     = "appserver/terraform.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform_user"
+  }
+}
+
+# ----------------------
+# provider configuration
+# ----------------------
+provider "aws" {
+  profile = "terraform_user"
+  region  = "ap-northeast-1"
+}
