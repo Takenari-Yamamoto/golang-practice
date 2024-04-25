720831a874b636abd954b070ac503bb3dcdd1236
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2024-04-25 11:11:24 +0900

    add comment

--- infra/golang-study/rds.tf
+++ infra/golang-study/rds.tf
@@ -1,17 +1,27 @@
+# Terraform を使用して AWS RDS (Amazon Relational Database Service) のデータベースインスタンスを管理するためのリソース
 resource "aws_db_instance" "golang-study-db" {
-  identifier        = "golang-study-rds"
-  engine            = "postgres"
-  instance_class    = "db.t3.micro"
+  # データベースインスタンスの一意の識別子です。AWS コンソール内でインスタンスを簡単に識別できるように
+  identifier = "golang-study-rds"
+  # 使用するデータベースの種類を指定します。ここでは postgres が使用されていますが、他にも mysql, oracle, sqlserver などが利用可能
+  engine = "postgres"
+  # データベースインスタンスのタイプ（サイズ）を指定します。ここでは db.t3.micro が使用されており、これは比較的小規模なインスタンス
+  instance_class = "db.t3.micro"
+  # データベースインスタンスに割り当てるストレージの量をギガバイト単位で指定
   allocated_storage = 20
-  username          = "dbuser"
-  password          = "dbpassword"
-  name              = "golang_study_db"
-
+  # データベースにアクセスするためのユーザ名とパスワードです。これらはデータベースへのログインに使用される
+  username = "dbuser"
+  password = "dbpassword"
+  #  データベースの名前を指定します。この名前は、特定のデータベースエンジンの要件に従う必要がある
+  name = "golang_study_db"
+  # データベースインスタンスに適用するセキュリティグループのIDのリストです。これにより、インスタンスへのネットワークアクセスを制御
   vpc_security_group_ids = [
     aws_security_group.golang-study-db-sg.id
   ]
+  # インスタンスが配置されるサブネットグループを指定します。これは、データベースが特定のVPC内の特定のサブネットに配置されることを保証
   db_subnet_group_name = aws_db_subnet_group.golang-study-db_subnet_group.name
-
+  # ルチアベイラビリティーゾーンの高可用性オプションを有効にするかどうかを指定します。
+  # これが true に設定されると、プライマリデータベースのスタンバイコピーが異なるアベイラビリティーゾーンに作成され、
+  # 障害発生時に自動的にフェイルオーバーが行われます
   multi_az = false
 
   tags = {
