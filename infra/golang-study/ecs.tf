5ab12bb8436086841928299802d5c9300019a950
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2024-04-26 20:20:08 +0900

    クラスタ

--- infra/golang-study/ecs.tf
+++ infra/golang-study/ecs.tf
@@ -2,3 +2,7 @@ resource "aws_ecr_repository" "golang-study-api-repo" {
   name                 = "golang-study-api-repo"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecs_cluster" "golang_study_cluster" {
+  name = "golang-study-cluster"
+}
