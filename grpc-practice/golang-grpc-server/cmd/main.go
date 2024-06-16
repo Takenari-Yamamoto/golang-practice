bacf35dd23684eac52b6e7e65c01676ade8795b2
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2024-06-16 13:30:29 +0900

    setup spanner

--- grpc-practice/golang-grpc-server/cmd/main.go
+++ grpc-practice/golang-grpc-server/cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	util "github.com/Takenari-Yamamoto/golang-grpc-server/util"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx := context.Background()
+	dbClient, err := util.NewSpannerClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer dbClient.Close()
+
+	fmt.Println("Spanner client is created successfully")
+
 }
---
+++ grpc-practice/golang-grpc-server/util/spanner.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/api/option"
+)
+
+const (
+	project  = "test-project"
+	instance = "test-instance"
+	database = "test-database"
+)
+
+func NewSpannerClient(ctx context.Context) (*spanner.Client, error) {
+
+	spannerEndpoint := "localhost:9010"
+	database := fmt.Sprintf("projects/%s/instances/%s/databases/%s", project, instance, database)
+
+	// クライアントオプションを設定
+	client, err := spanner.NewClient(ctx, database, option.WithEndpoint(spannerEndpoint), option.WithoutAuthentication())
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	return client, nil
+}
